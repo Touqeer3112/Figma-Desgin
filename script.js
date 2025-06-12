aa9b47be9c949f2f268eda65fa1ef8533b785761
Muhammad Touqeer <31.tmouqeer@gmail.com>
2025-06-12 06:29:42 -0700

    week1

---
+++ script.js
@@ -0,0 +1,6 @@
+// const toggleBtn = document.getElementById('toggleCategories');
+// const dropdown = document.getElementById('categoriesDropdown');
+
+// toggleBtn.addEventListener('click', () => {
+//   dropdown.style.display = dropdown.style.display === 'block' ? 'none' : 'block';
+// });
